020f45fa8d99e0a1ced5025952027aecfacf6ad1
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2021-08-27 11:54:46 +0900

    OE-Japanへ変更

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
+import logo from "../img/logo.svg";
+import facebook from "../img/social/facebook.svg";
 /* import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg' */
-import github from '../img/github-icon.svg'
+import github from "../img/github-icon.svg";
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="OE Japan"
-            style={{ width: '14em', height: '10em' }}
-          />
+          <img src={logo} alt="OE Japan" style={{ width: "14em", height: "10em" }} />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
@@ -55,7 +51,7 @@ const Footer = class extends React.Component {
                         News and Updates
                       </Link>
                     </li>
-{/*                     <li>
+                    {/*                     <li>
                       <Link className="navbar-item" to="/contact">
                         Contact
                       </Link>
@@ -64,17 +60,10 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a
-                  title="facebook"
-                  href="https://www.facebook.com/JapanOCWConsortium/"
-                >
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                <a title="facebook" href="https://www.facebook.com/JapanOCWConsortium/">
+                  <img src={facebook} alt="Facebook" style={{ width: "1em", height: "1em" }} />
                 </a>
-{/*                 <a title="twitter" href="https://twitter.com">
+                {/*                 <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -90,25 +79,16 @@ const Footer = class extends React.Component {
                   />
                 </a>
                  */}
-                <a
-                  titel="Github"
-                  href="https://github.com/Takaya-Yamazato/oe-japan-cms"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={github}
-                    alt="Github"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                <a titel="Github" href="https://github.com/OE-Japan/oe-japan-cms" target="_blank" rel="noopener noreferrer">
+                  <img src={github} alt="Github" style={{ width: "1em", height: "1em" }} />
                 </a>
               </div>
             </div>
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
