bc3a1c4a39f9ffe95bfb620b180b07b17999a3bf
shigejam <shige@jamsquare.org>
2021-12-29 15:57:08 +0900

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -73,7 +73,7 @@ const IndexPageTemplate = ({ data }) => (
                     <section className="section">
                       <div className="has-text-centered">
                         <div style={{width: "240px", display: "inline-block"}} >
-                            <StaticImage src="../../static/img/coffee.png" alt="Open Education" placeholder="blurred" />
+                            <StaticImage src="../../static/img/oe.png" alt="Open Education" placeholder="blurred" />
                         </div>
                       </div>
                       <br />
@@ -84,7 +84,7 @@ const IndexPageTemplate = ({ data }) => (
                     <section className="section">
                       <div className="has-text-centered">
                         <div style={{width: "240px", display: "inline-block"}} >
-                            <StaticImage src="../../static/img/coffee-gear.png" alt="Open Educational Resources" placeholder="blurred" />
+                            <StaticImage src="../../static/img/oer.png" alt="Open Educational Resources" placeholder="blurred" />
                         </div>
                       </div>
                       <br />
@@ -98,7 +98,7 @@ const IndexPageTemplate = ({ data }) => (
                     <section className="section">
                       <div className="has-text-centered">
                         <div style={{width: "240px", display: "inline-block"}} >
-                            <StaticImage src="../../static/img/tutorials.png" alt="OpenCourseWare" placeholder="blurred" />
+                            <StaticImage src="../../static/img/ocw.png" alt="OpenCourseWare" placeholder="blurred" />
                         </div>
                       </div>
                       <br />
@@ -109,7 +109,7 @@ const IndexPageTemplate = ({ data }) => (
                     <section className="section">
                       <div className="has-text-centered">
                         <div style={{width: "240px", display: "inline-block"}} >
-                            <StaticImage src="../../static/img/meeting-space.png" alt="MOOCs" placeholder="blurred" />
+                            <StaticImage src="../../static/img/mooc.png" alt="MOOCs" placeholder="blurred" />
                         </div>
                       </div>
                       <br />
