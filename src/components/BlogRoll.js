cb891f788aad4f9d1f9aba09573de3709bdb61eb
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-06-28 16:22:59 +0900

    6/28

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,47 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
-class BlogRoll extends React.Component {
+class BlogRollTemplate extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost == 'true'
-                    ? 'is-featured'
-                    : 'featured'
-                }`}
-              >
-                {/* {post.frontmatter.featuredimage ? ( */}
-                <div className="featured-thumbnail">
-                  <PreviewCompatibleImage
-                    imageInfo={{
-                      image: post.frontmatter.featuredimage,
-                      alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                    }}
-                  />
-                </div>
-                {/* ) : null} */}
-                <p className="post-meta">
-                  <Link
-                    className="title has-text-primary is-size-4"
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <span className="subtitle is-size-6 is-block">
-                    {post.frontmatter.date}
-                  </span>
-                </p>
+              <article className={`blog-list-item tile is-child box notification ${post.frontmatter.featuredpost ? "is-featured" : ""}`}>
+                <header>
+                  {post.frontmatter.featuredimage ? (
+                    <div className="featured-thumbnail">
+                      <PreviewCompatibleImage
+                        imageInfo={{
+                          image: post.frontmatter.featuredimage,
+                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                          width: post.frontmatter.featuredimage.childImageSharp.gatsbyImageData.width,
+                          height: post.frontmatter.featuredimage.childImageSharp.gatsbyImageData.height,
+                        }}
+                      />
+                    </div>
+                  ) : null}
+                  <p className="post-meta">
+                    <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
+                  </p>
+                </header>
                 <p>
                   {post.excerpt}
                   <br />
@@ -54,7 +47,7 @@ class BlogRoll extends React.Component {
             </div>
           ))}
       </div>
-    )
+    );
   }
 }
 
@@ -64,17 +57,14 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
-export default function BlogRollQuery() {
+export default function BlogRoll() {
   return (
     <StaticQuery
       query={graphql`
-        query {
-          allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
-            filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-          ) {
+        query BlogRollQuery {
+          allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, filter: { frontmatter: { templateKey: { eq: "blog-post" } } }) {
             edges {
               node {
                 excerpt(pruneLength: 400)
@@ -89,11 +79,7 @@ export default function BlogRollQuery() {
                   featuredpost
                   featuredimage {
                     childImageSharp {
-                      gatsbyImageData(
-                        width: 240
-                        quality: 100
-                        layout: CONSTRAINED
-                      )
+                      gatsbyImageData(width: 120, quality: 100, layout: CONSTRAINED)
                     }
                   }
                 }
@@ -102,7 +88,7 @@ export default function BlogRollQuery() {
           }
         }
       `}
-      render={(data, count) => <BlogRoll data={data} count={count} />}
+      render={(data, count) => <BlogRollTemplate data={data} count={count} />}
     />
-  )
+  );
 }
