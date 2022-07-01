5649a3d078ba30bc2239fb81e06ad10e5ad3ca7c
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-07-01 21:06:25 +0900

    Fixed

--- src/templates/history-page.js
+++ src/templates/history-page.js
@@ -1,30 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import { Helmet } from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
 
-export const HistoryTemplate = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  helmet,
-}) => {
-  const PostContent = contentComponent || Content
+// eslint-disable-next-line
+export const HistoryTemplate = ({ content, contentComponent, description, tags, title, helmet }) => {
+  const PostContent = contentComponent || Content;
 
   return (
     <section className="section">
-      {helmet || ''}
+      {helmet || ""}
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
             <p>{description}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
@@ -43,8 +35,8 @@ export const HistoryTemplate = ({
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 HistoryTemplate.propTypes = {
   content: PropTypes.node.isRequired,
@@ -52,10 +44,10 @@ HistoryTemplate.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string,
   helmet: PropTypes.object,
-}
+};
 
 const History = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -66,28 +58,25 @@ const History = ({ data }) => {
         helmet={
           <Helmet>
             <title>History | OE Japan</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
-            />
+            <meta name="description" content={`${post.frontmatter.description}`} />
           </Helmet>
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
-  )
-}
+  );
+};
 
 History.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
-}
+};
 
-export default History
+export default History;
 
-export const pageQuery = graphql`
+export const HistoryPageQuery = graphql`
   query HistoryByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
@@ -100,4 +89,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
