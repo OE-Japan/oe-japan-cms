415306bd80855a22096942d451d01f96b531e05f
Takaya YAMAZATO <yamazato@ilas.nagoya-u.ac.jp>
2024-08-06 19:33:55 +0900

    package updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ import github from '../img/github-icon.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer className="footer has-text-white-ter">
         <div className="content has-text-centered">
           <img
             src={logo}
@@ -18,11 +18,11 @@ const Footer = class extends React.Component {
             style={{ width: '14em', height: '10em' }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-text-white-ter">
+          <div className="container has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
-                <section className="menu">
+                <section>
                   <ul className="menu-list">
                     <li>
                       <Link to="/" className="navbar-item">
@@ -106,6 +106,7 @@ const Footer = class extends React.Component {
             </div>
           </div>
         </div>
+        <br />
       </footer>
     )
   }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ const TemplateWrapper = ({ children }) => {
   return (
     <div>
       <Helmet>
-        <html lang="ja" />
+        <html lang="ja" data-theme="light" />
         <title>{title}</title>
         <meta name="description" content={description} />
 
